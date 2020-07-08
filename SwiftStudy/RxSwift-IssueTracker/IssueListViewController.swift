1cebcbfcf74802b7effa5d7004fd7ff7d008a17c
PD Jell <mystral7@naver.com>
2020-07-08 17:44:24 +0900

    add moya.

---
+++ SwiftStudy/RxSwift-IssueTracker/IssueListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  IssueListViewController.swift
+//  SwiftStudy
+//
+//  Created by YooHG on 7/8/20.
+//  Copyright © 2020 Jell PD. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Moya
+
+class IssueListViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchBar: UISearchBar {
+        return searchController.searchBar
+    }
+    let disposeBag = DisposeBag()
+    var provider: MoyaProvider<Issue_Github>!
+    var latestRepositoryName: Observable<String> {
+        return searchBar
+            .rx.text
+            .orEmpty
+            .debounce(DispatchTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableHeaderView = searchBar
+        setupRx()
+    }
+    
+    func setupRx() {
+        // Provider 생성
+        provider = MoyaProvider<Issue_Github>()
+        
+        // 유저가 셀을 클릭했을 때 테이블 뷰에게 알려줌.
+        // 만약 키보드가 있다면 숨김.
+        tableView
+            .rx.itemSelected
+            .subscribe(onNext: { IndexPath in
+                if self.searchBar.isFirstResponder == true {
+                    self.view.endEditing(true)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //                }
         //            })
         
-        menuArray = NSArray.init(objects: "구구단", "realm", "IBDesignable", "Drawing", "Category", "SwiftUI", "Landmark list", "Local Push", "Rx-Github", "Rx-City", "Rx-chameleon")
+        menuArray = NSArray.init(objects: "구구단", "realm", "IBDesignable", "Drawing", "Category", "SwiftUI", "Landmark list", "Local Push", "Rx-Github", "Rx-City", "Rx-chameleon", "Rx-issue")
     }
     
     override func didReceiveMemoryWarning() {
@@ -90,6 +90,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             self.present(vc, animated: true, completion: nil)
         case 10:
             let vc = self.storyboard?.instantiateViewController(withIdentifier: "rxchameleon") as! RXChameleonViewController
+            self.present(vc, animated: true, completion: nil)
+        case 11:
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "rxissue") as! IssueListViewController
             self.present(vc, animated: true, completion: nil)
         default:
             return;
