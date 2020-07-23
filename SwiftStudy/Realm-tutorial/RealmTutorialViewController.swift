3ce8a90916ae289ba88906790b3b685fd6954954
PD Jell <mystral7@naver.com>
2020-07-23 18:06:10 +0900

    add tutorial-basic.

--- SwiftStudy/Realm-tutorial/RealmTutorialViewController.swift
+++ SwiftStudy/Realm-tutorial/RealmTutorialViewController.swift
@@ -7,7 +7,46 @@
 //
 
 import UIKit
+import Foundation
+import RealmSwift
+
+class Dog: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+}
+
+class Person: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var picture: Data? = nil
+    let dogs = List<Dog>()
+}
 
 class RealmTutorialViewController: UIViewController {
-    
+    override func viewDidLoad() {
+        let myDog = Dog()
+        myDog.name = "Rex"
+        myDog.age = 1
+        print("name of dog: \(myDog.name)")
+        
+        let realm = try! Realm()
+        
+        let puppies = realm.objects(Dog.self).filter("age < 2")
+        print(puppies.count)
+        
+        try! realm.write {
+            realm.add(myDog)
+        }
+        
+        print(puppies.count)
+        
+        DispatchQueue(label: "background").async {
+            autoreleasepool {
+                let realm = try! Realm()
+                let theDog = realm.objects(Dog.self).filter("age == 1").first
+                try! realm.write {
+                    theDog!.age = 3
+                }
+            }
+        }
+    }
 }
