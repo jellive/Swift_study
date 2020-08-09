1274c17f6b8c74c62c7e161f2607ec5e78096fe4
Jell PD <mystral7@naver.com>
2020-08-09 13:29:25 +0900

    move tableview source.

--- SwiftStudy/Realm-menu/RealmMenuViewController.swift
+++ SwiftStudy/Realm-menu/RealmMenuViewController.swift
@@ -11,6 +11,17 @@ import RxSwift
 import RxCocoa
 
 class RealmMenuViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rx.setDelegate(self).disposed(by: bag)
+        
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -19,14 +30,4 @@ class RealmMenuViewController: UIViewController, UIScrollViewDelegate, UITableVi
         return UITableViewCell()
     }
     
-    
-    @IBOutlet var tableView: UITableView!
-    
-    let bag = DisposeBag()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.rx.setDelegate(self).disposed(by: bag)
-        
-    }
 }
