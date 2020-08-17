5b75015860d5c9ea39cdf8e94291b380ec84d268
Jell PD <mystral7@naver.com>
2020-08-17 15:42:08 +0900

    fix RxTableViewCell.

--- SwiftStudy/Realm-menu/RealmMenuViewController.swift
+++ SwiftStudy/Realm-menu/RealmMenuViewController.swift
@@ -11,7 +11,7 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class RealmMenuViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
+class RealmMenuViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var tableView: UITableView!
     
@@ -24,22 +24,28 @@ class RealmMenuViewController: UIViewController, UIScrollViewDelegate, UITableVi
         super.viewDidLoad()
         
 //        tableView.rx.setDelegate(self).disposed(by: bag)
-        tableView.delegate = self
+//        tableView.delegate = self
          
+        let menuOb = Observable.of(menuArr)
+        menuOb.bind(to: tableView.rx.items) { (tableView: UITableView, index: Int, element: String) -> UITableViewCell in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell()}
+            cell.textLabel?.text = element
+            return cell
+        }.disposed(by: bag)
         tableView.rx.itemSelected.subscribe(onNext: { indexPath in
             print("\(indexPath)")
         })
         .disposed(by: bag)
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return menuArr.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
-        cell.detailTextLabel?.text = menuArr[indexPath.row]
-        return cell
-    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return menuArr.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+//        cell.detailTextLabel?.text = menuArr[indexPath.row]
+//        return cell
+//    }
     
     
 }
