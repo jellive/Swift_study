ebecc52e6d4fcafd5ce696d5631c3dde4c14b53a
PD Jell <mystral7@naver.com>
2020-08-22 16:21:18 +0900

    Implement RealmMenuViewController.

--- SwiftStudy/Realm-menu/RealmMenuViewController.swift
+++ SwiftStudy/Realm-menu/RealmMenuViewController.swift
@@ -34,7 +34,16 @@ class RealmMenuViewController: UIViewController, UIScrollViewDelegate {
         
         tableView.rx.itemSelected.subscribe(onNext: { indexPath in
             DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-                print("\(indexPath)")
+                switch indexPath.row {
+                    case 0:
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "realm") as! RealmViewController
+                        self.present(vc, animated: true){}
+                    case 1:
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "realm_tutorial") as! RealmTutorialViewController
+                        self.present(vc, animated: true){}
+                    default:
+                    print("\(indexPath)")
+                }
             })
         })
         .disposed(by: bag)
