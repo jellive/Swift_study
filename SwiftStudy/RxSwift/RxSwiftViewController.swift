f05b1826170b6eab81fc90108b5579db2bca7f86
PD Jell <mystral7@naver.com>
2020-08-26 17:21:55 +0900

    add rxtableview.

--- SwiftStudy/RxSwift/RxSwiftViewController.swift
+++ SwiftStudy/RxSwift/RxSwiftViewController.swift
@@ -12,4 +12,18 @@ import RxCocoa
 
 class RxSwiftViewController: UIViewController {
     
+    let menuArr = ["Chameleon","city","github","Issuetracker"]
+    let bag = DisposeBag()
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Observable.of(menuArr).bind(to: tableView.rx.items) {(tableView: UITableView, index: Int, element: String) -> UITableViewCell in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {return UITableViewCell()}
+            cell.textLabel?.text = element
+            return cell
+        }
+        .disposed(by: bag)
+    }
 }
