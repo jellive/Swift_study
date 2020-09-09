ccb5b4644f767c5b45a3edf58421fc66e4df6545
PD Jell <mystral7@naver.com>
2020-09-09 14:28:11 +0900

    basic subscribe.

--- SwiftStudy/RxSwift/Rxswift-sketchbook/RxSwiftSketchbookViewController.swift
+++ SwiftStudy/RxSwift/Rxswift-sketchbook/RxSwiftSketchbookViewController.swift
@@ -10,7 +10,21 @@ import RxSwift
 import RxCocoa
 
 class RxSwiftSketchbookViewController: UIViewController {
+    
+    let bag = DisposeBag()
+    
     override func viewDidLoad() {
         print("Hello world!")
+        
+        let a = Observable.just("a")
+        
+        a.subscribe(onNext: {print("onNext: \($0)")}, onError: {error in
+            print("onError: \(error)")
+        }, onCompleted: {
+            print("onCompleted")
+            
+        }, onDisposed: {
+            print("onDisposed")
+        }).disposed(by: bag)
     }
 }
