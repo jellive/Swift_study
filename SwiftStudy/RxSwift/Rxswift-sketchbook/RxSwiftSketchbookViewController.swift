c31dc27ccfd3ea525cc6cfa71b5353e393c12519
PD Jell <mystral7@naver.com>
2020-09-09 16:21:06 +0900

    add behavior.

--- SwiftStudy/RxSwift/Rxswift-sketchbook/RxSwiftSketchbookViewController.swift
+++ SwiftStudy/RxSwift/Rxswift-sketchbook/RxSwiftSketchbookViewController.swift
@@ -11,20 +11,47 @@ import RxCocoa
 
 class RxSwiftSketchbookViewController: UIViewController {
     
+    let behavior: BehaviorSubject<Int> = BehaviorSubject(value: 0)
+    
     let bag = DisposeBag()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         print("Hello world!")
         
-        let a = Observable.just("a")
+        let z = 1
         
-        a.subscribe(onNext: {print("onNext: \($0)")}, onError: {error in
+        let a = Observable.from(optional: z)
+        
+        
+        a.subscribe(onNext: {
+            print("onNext: \($0)")
+        }, onError: {error in
             print("onError: \(error)")
         }, onCompleted: {
             print("onCompleted")
-            
         }, onDisposed: {
             print("onDisposed")
         }).disposed(by: bag)
+        
+        behavior.subscribe(onNext: {
+            print("behavior: \($0)")
+            }).disposed(by: bag)
+        
+        behavior.onNext(try! behavior.value() + 1)
+        
+        
+        behavior.onNext(try! behavior.value() + 1)
+        behavior.onNext(try! behavior.value() + 1)
+        behavior.onNext(try! behavior.value() + 1)
+        behavior.onNext(try! behavior.value() + 1)
+        
+
+//        a.bind(to: behavior).disposed(by: bag)
+//
+//        z = 100
+        
+        behavior.onNext(100)
+
     }
 }
