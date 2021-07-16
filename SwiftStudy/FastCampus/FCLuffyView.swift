37ab222f83bcbcc5579204cc6d8d06ac1396ed5e
PD Jell <>
2021-07-16 15:49:33 +0900

    add alert.

--- SwiftStudy/FastCampus/FCLuffyView.swift
+++ SwiftStudy/FastCampus/FCLuffyView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct FCLuffyView: View {
     @State var price = 100000
+    @State private var showingAlert = false
     
     var body: some View {
         if #available(iOS 14.0, *) {
@@ -23,9 +24,12 @@ struct FCLuffyView: View {
                 Text("₩\(price)").font(.system(size: 50))
                 Spacer()
                 Button(action: {
+                    self.showingAlert = true
                     self.price = 2000000
                 }, label: {
                     Text("reload")
+                }).alert(isPresented: $showingAlert, content: {
+                    .init(title: Text("hello"), message: Text("First App!"), dismissButton: .default(Text("ok")))
                 })
                 
             }
