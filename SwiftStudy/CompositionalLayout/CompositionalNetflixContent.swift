1fc8a0feb0bd150c16cecc91db1e134b8a6a25ae
Jell <hangoon.yoo@finiview.com>
2024-01-09 17:55:45 +0900

    add Compositional Layout Netflix view(basic)

---
+++ SwiftStudy/CompositionalLayout/CompositionalNetflixContent.swift
@@ -0,0 +1,9 @@
+//
+//  CompositionalNetflixContent.swift
+//  SwiftStudy
+//
+//  Created by HGYoo-visualsyn on 1/9/24.
+//  Copyright © 2024 Jell PD. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftStudy/CompositionalLayout/ContentCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ContentCollectionViewCell.swift
+//  SwiftStudy
+//
+//  Created by HGYoo-visualsyn on 1/9/24.
+//  Copyright © 2024 Jell PD. All rights reserved.
+//
+
+import Foundation
