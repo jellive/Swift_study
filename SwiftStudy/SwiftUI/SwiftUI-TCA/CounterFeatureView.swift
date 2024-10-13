ca4733308a744eb1fb62210cb0de213a903a5b3c
Jell <mystral7@naver.com>
2024-10-14 08:27:15 +0900

    [SwiftUI] add TCA tutorial

---
+++ SwiftStudy/SwiftUI/SwiftUI-TCA/CounterFeatureView.swift
@@ -0,0 +1,38 @@
+//
+//  CounterFeatureView.swift
+//  SwiftStudy
+//
+//  Created by Jell PD on 10/13/24.
+//  Copyright © 2024 Jell PD. All rights reserved.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct CounterFeatureView: View {
+    let store: StoreOf<CounterFeature>
+    
+    var body: some View {
+        Form {
+            Section {
+                Text("\(store.count)")
+                Button("Decrement") {store.send(.decrementButtonTapped)}
+                Button("Increment") {store.send(.incrementButtonTapped)}
+            }
+            
+            Section {
+                Button("Number fact") { store.send(.numberFactButtonTapped)}
+            }
+            
+            if let fact = store.numberFact {
+                Text(fact)
+            }
+        }
+    }
+}
+
+#Preview {
+    CounterFeatureView(store: Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+    })
+}
